3a814f1d874eecfb486e066f815d6521f6becbf7
Pedro <ppereiramartins@gmail.com>
2016-09-19 09:45:58 +0100

    Refactor form, add flash alerts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,9 @@
 class PostsController < ApplicationController
   def index
-    @post = Post.new
+    @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -16,7 +15,20 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
-    redirect_to @post
+
+    if @post.save
+      flash[:success] = "Your post has been saved."
+      redirect_to @post
+    else
+      flash[:alert] = "There was a problem saving your post. Please try again."
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  def destroy
   end
 
 
@@ -24,5 +36,9 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :body, :image)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
